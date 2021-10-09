a58818448bf27cda6ed70386fcee0112a7a5abdf
Dylan-Allison-Damitz <dylandamitz@gmail.com>
2021-10-09 13:12:13 -0700

    add intial Clients controller class

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controller
+  {
+    private readonly HairSalonContext _db;
+
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
